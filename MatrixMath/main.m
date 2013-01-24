558db80707d897fab592d22f8495caf9f5c6c953
Alex Nichol <alexnichol@comcast.net>
2013-01-24 16:25:58 -0500

    Initial Commit

---
+++ MatrixMath/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MatrixMath
+//
+//  Created by Alex Nichol on 1/24/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
